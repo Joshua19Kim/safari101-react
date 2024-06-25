c36ddc34b640b6c25d41d88db3c5e514d6e2d00b
Joshua Kim <bki42@uclive.ac.nz>
2024-06-25 23:37:00 +1200

    Added nav bar with logo on main page

--- tt-app/src/components/Main.tsx
+++ tt-app/src/components/Main.tsx
@@ -1,5 +1,32 @@
+import React, { useRef, useEffect } from "react";
+import '../assets/css/Main.css';
+import SearchAppBar from "./SearchAppBar";
+
+
 const Main = () => {
-    return (<h1>Tanzania Tour Web site</h1>)
+
+    const pageHeader = useRef<HTMLDivElement>(null);
+
+
+
+
+
+    return (
+        <>
+            <SearchAppBar/>
+            <div
+                style={{
+                    backgroundImage: `url(${require('../assets/img/daniel-olahh.jpg')})`,
+                }}
+                className="page-background"
+                data-parallax={true}
+                ref={pageHeader}
+
+            >
+            </div>
+        </>
+
+)
 }
 
 export default Main;
---
+++ tt-app/src/components/SearchAppBar.tsx
@@ -0,0 +1,132 @@
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import logoImage from '../assets/img/tazania101Logo.jpeg';
+import {Button, List } from 'reactstrap';
+import {Divider, ListItem, ListItemButton, ListItemText} from '@mui/material';
+
+const Logo = styled('img')({
+    maxHeight: 40,
+})
+
+const navItems = ['Home', 'About', 'Contact'];
+
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+    },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    width: '100%',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                width: '20ch',
+            },
+        },
+    },
+}));
+
+export default function SearchAppBar() {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="fixed" sx={{ backgroundColor: 'transparent', boxShadow: 'none', maxHeight: 50 }}>
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="open drawer"
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+
+                    <Box sx={{ flexGrow: 500, textAlign: 'center' }}>
+                        <Logo src={logoImage} alt="Logo" />
+                    </Box>
+
+                    <Box sx={{ display:'flex', flexGrow: 1, textAlign: 'right' }}>
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            component="a"
+                            href="#app-bar-with-responsive-menu"
+                            sx={{
+                                mr: 2,
+                                display: { xs: 'none', md: 'flex' },
+                                fontFamily: 'monospace',
+                                fontWeight: 400,
+                                color: 'inherit',
+                                textDecoration: 'none',
+                                marginRight: '3rem',
+                            }}
+                        >
+                            Make A Request
+                        </Typography>
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            component="a"
+                            href="#app-bar-with-responsive-menu"
+                            sx={{
+                                mr: 2,
+                                display: { xs: 'none', md: 'flex' },
+                                fontFamily: 'monospace',
+                                fontWeight: 400,
+                                color: 'inherit',
+                                textDecoration: 'none',
+                            }}
+                        >
+                            Contact
+                        </Typography>
+                    </Box>
+
+
+                    <Search>
+                        <SearchIconWrapper>
+                            <SearchIcon />
+                        </SearchIconWrapper>
+                        <StyledInputBase
+                            placeholder="Search…"
+                            inputProps={{ 'aria-label': 'search' }}
+                        />
+                    </Search>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+}
