a7aec51bb645ecafa78e9af3e6bf7965331ea649
Joshua Kim <bki42@uclive.ac.nz>
2024-07-15 21:15:10 +1200

    testing without nav bar

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -33,7 +33,7 @@ const LandingPage = () => {
 
     return (
         <>
-            <SearchAppBar />
+            {/*<SearchAppBar />*/}
 
             <Container>
                 <RequestBox image={backgroundImage} />
